2ff8e3b825b52cdb29b2bc7f6f2903f6f5d29491
mt4real <54484589+mt4real@users.noreply.github.com>
2021-04-12 18:30:16 +0100

    my tinder clone website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    //BEm class naming convention
+    <div className="app">
+      {/* Header */}
+        <Header/>
+      {/* TinderCards*/}
+      {/* SwipeButtons*/}
+   </div>
+  );
+}
+
+export default App;
